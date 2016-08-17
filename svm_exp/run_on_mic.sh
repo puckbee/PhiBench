e19b470e9b5dc9e8657ee44ade616dedd8ca52cb
puckbee <puckbee@gmail.com>
2016-08-17 05:55:01 -0400

    Add svm Codes

---
+++ svm_exp/run_on_mic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$2" = "cilk" ]; then
+export CILK_NWORKERS="$3"
+echo CILK_NWORKERS: "$3"
+else 
+export KMP_AFFINITY=scatter
+export OMP_SCHEDULE=dynamic
+#export OMP_WAIT_POLICY=PASSIVE
+#export KMP_SETTINGS=1
+#export KMP_BLOCKTIME=0
+export OMP_NUM_THREADS="$3"
+echo OMP_NUM_THREADS: "$3"
+fi
+#./"$1"."$2" vtune.txt "$1".param
+./"$1"."$2" -c 1 -g 0.00020 -o svm.mdl vtune.txt
